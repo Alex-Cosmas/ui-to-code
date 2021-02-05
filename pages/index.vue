3d697cade359a742a8b8f6c7b5068b406b40b1ad
Alex Cosmas <2alexcosmas@gmail.com>
2021-02-05 23:11:56 +0300

    Grid layout

---
+++ pages/index.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="">
+    <main class="container mx-auto px-10 bg-gray-50">
+      <div class="text-center py-10 mb-5">
+        <h1 class="text-4xl">UI Designs translated into code</h1>
+        Click the links to go to the full individual pages.
+      </div>
+
+      <div class="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 ">
+        <div class="image">
+          <div class="relative pb-96 rounded-lg overflow-hidden">
+            <img
+              class=" h-full w-full object-cover"
+              src="https://uidcomponents.netlify.app/accordion%20menu/day_1175.png"
+              alt=""
+            />
+          </div>
+          <div class="relative px-4 -mt-16">
+            <div class="bg-white p-6 rounded-lg">
+              <div
+                class="text-gray-600  text-xs uppercase font-semibold tracking-wide"
+              >
+                <p class="float-right">
+                  Description:
+                  <span class="bg-gray-900 rounded-md px-4 py-1 text-white mx-1"
+                    >PROPOSAL</span
+                  >
+                </p>
+              </div>
+              <h4 class="uppercase font-semibold tracking-wide">Header</h4>
+              <div class="text-gray-600">
+                <p class="py-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Mollitia, ab. Aliquid similique distinctio delectus
+                  praesentium iusto dolore adipisci incidunt, odio recusandae
+                  neque facere, laborum saepe cumque animi, atque iure deserunt!
+                </p>
+              </div>
+
+              <div>Link</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="image">
+          <div class="relative pb-96 rounded-lg overflow-hidden">
+            <img
+              class=" h-full w-full object-cover"
+              src="https://uidcomponents.netlify.app/accordion%20menu/day_1175.png"
+              alt=""
+            />
+          </div>
+          <div class="relative px-4 -mt-16">
+            <div class="bg-white p-6 rounded-lg">
+              <div
+                class="text-gray-600  text-xs uppercase font-semibold tracking-wide"
+              >
+                <p class="float-right">
+                  Description:
+                  <span class="bg-gray-900 rounded-md px-4 py-1 text-white mx-1"
+                    >PROPOSAL</span
+                  >
+                </p>
+              </div>
+              <h4 class="uppercase font-semibold tracking-wide">Header</h4>
+              <div class="text-gray-600">
+                <p class="py-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Mollitia, ab. Aliquid similique distinctio delectus
+                  praesentium iusto dolore adipisci incidunt, odio recusandae
+                  neque facere, laborum saepe cumque animi, atque iure deserunt!
+                </p>
+              </div>
+
+              <div>Link</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="image">
+          <div class="relative pb-96 rounded-lg overflow-hidden">
+            <img
+              class=" h-full w-full object-cover"
+              src="https://uidcomponents.netlify.app/accordion%20menu/day_1175.png"
+              alt=""
+            />
+          </div>
+          <div class="relative px-4 -mt-16">
+            <div class="bg-white p-6 rounded-lg">
+              <div
+                class="text-gray-600  text-xs uppercase font-semibold tracking-wide"
+              >
+                <p class="float-right">
+                  Description:
+                  <span class="bg-gray-900 rounded-md px-4 py-1 text-white mx-1"
+                    >PROPOSAL</span
+                  >
+                </p>
+              </div>
+              <h4 class="uppercase font-semibold tracking-wide">Header</h4>
+              <div class="text-gray-600">
+                <p class="py-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Mollitia, ab. Aliquid similique distinctio delectus
+                  praesentium iusto dolore adipisci incidunt, odio recusandae
+                  neque facere, laborum saepe cumque animi, atque iure deserunt!
+                </p>
+              </div>
+
+              <div>Link</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="image">
+          <div class="relative pb-96 rounded-lg overflow-hidden">
+            <img
+              class=" h-full w-full object-cover"
+              src="https://uidcomponents.netlify.app/accordion%20menu/day_1175.png"
+              alt=""
+            />
+          </div>
+          <div class="relative px-4 -mt-16">
+            <div class="bg-white p-6 rounded-lg">
+              <div
+                class="text-gray-600  text-xs uppercase font-semibold tracking-wide"
+              >
+                <p class="float-right">
+                  Description:
+                  <span class="bg-gray-900 rounded-md px-4 py-1 text-white mx-1"
+                    >PROPOSAL</span
+                  >
+                </p>
+              </div>
+              <h4 class="uppercase font-semibold tracking-wide">Header</h4>
+              <div class="text-gray-600">
+                <p class="py-2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Mollitia, ab. Aliquid similique distinctio delectus
+                  praesentium iusto dolore adipisci incidunt, odio recusandae
+                  neque facere, laborum saepe cumque animi, atque iure deserunt!
+                </p>
+              </div>
+
+              <div>Link</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
